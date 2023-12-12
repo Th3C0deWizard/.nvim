9abc661c610fa0b2a6d954c073d30c96dec29860
Carlos Paez <carlosa.paezc@autonoma.edu.co>
2023-12-11 22:05:41 -0500

    autopairs cmp fix

--- lua/cheto/plugins/bufferline.lua
+++ lua/cheto/plugins/bufferline.lua
@@ -1,11 +1,11 @@
 return {
-  'akinsho/bufferline.nvim',
-  version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    require("bufferline").setup({})
-    
-    vim.keymap.set("n", "<leader>c", vim.cmd.BufferLineCloseOthers)
-    vim.keymap.set("n", "<leader><Tab>", vim.cmd.BufferLineCycleNext)
-  end
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("bufferline").setup({})
+
+		vim.keymap.set("n", "<leader>c", vim.cmd.BufferLineCloseOthers)
+		vim.keymap.set("n", "<leader><Tab>", vim.cmd.BufferLineCycleNext)
+	end,
 }
