50de143b7e5fd88ce7d4b29d333f57e955f99c90
Carlos Paez <carlosa.paezc@autonoma.edu.co>
2023-12-11 21:29:24 -0500

    almost ready

--- lua/cheto/plugins/treesitter.lua
+++ lua/cheto/plugins/treesitter.lua
@@ -1,21 +1,23 @@
-return { 
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  config = function()
-    local TSconfig = require("nvim-treesitter.configs")
-    TSconfig.setup({
-      ensure_installed = {
-      },
-      highlight = {
-        enable = true
-      },
-      indent = {
-        enable = true
-      },
-      autotag = {
-        enable = true,
-      }
-    })
-  end,
+return {
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local TSconfig = require("nvim-treesitter.configs")
+		TSconfig.setup({
+			ensure_installed = {},
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			autotag = {
+				enable = true,
+			},
+		})
+	end,
 }
